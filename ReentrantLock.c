596d80fcd6c829b2ee60b51b76ed9546f81dc302
xiangshi <xiang.shi@oriente.com>
2020-12-18 19:53:41 +0800

    first commit

---
+++ ReentrantLock.c
@@ -0,0 +1,158 @@
+#include "ReentrantLock.h"
+
+static int compareAndSetState(int *this,int expect,int update);
+static void fairLock(LOCK);
+static void nonfairLock(LOCK);
+static void acquire(LOCK,ACQUIRES);
+static int tryAcquire(LOCK,ACQUIRES);
+static int fairTryAcquire(LOCK,ACQUIRES);
+static int nonfairTryAcquire(LOCK,ACQUIRES);
+static int hasQueuedPredecessors(LOCK);
+static int acquireQueued(LOCK,NODE,ACQUIRES);
+static int shouldParkAfterFailedAcquire(LOCK_NODE *pred, LOCK_NODE *node);
+static int parkAndCheckInterrupt();
+
+
+void lock(LOCK){
+    if(IS_FAIR){
+        fairLock(lock);
+    }else {
+        nonfairLock(lock);
+    }
+}
+
+void tryLock(LOCK){
+
+}
+
+void unlock(LOCK){
+
+}
+
+static int compareAndSetState(int *this,int expect,int update){
+    return __sync_val_compare_and_swap(this,expect,update);
+}
+
+static void fairLock(LOCK){
+    acquire(lock,1);
+}
+
+static void nonfairLock(LOCK){
+    if(compareAndSetState(&lock->cas,0,1)){
+        lock->exclusive_owner_thread = getpid();
+    }else{
+        acquire(lock,1);
+    }
+}
+
+static void acquire(LOCK,ACQUIRES){
+    if( tryAcquire(lock,acquires) == 0 &&
+        ){
+
+    }
+}
+
+static int tryAcquire(LOCK,ACQUIRES){
+    if(IS_FAIR){
+        fairTryAcquire(lock,acquires);
+    }else{
+        nonfairTryAcquire(lock,acquires);
+    }
+}
+
+static int fairTryAcquire(LOCK,ACQUIRES){
+    pid_t current_thread = getpid();
+    if(lock->state == 0){ //当前锁没有枷锁
+        //再次检查是否有其他线程抢占锁
+        if(hasQueuedPredecessors(lock) == 0 && //公平锁需要考虑等待队列中是否有其他等待的node
+            compareAndSetState(&lock->cas,0,acquires)){
+                lock->exclusive_owner_thread = getpid();
+                return 1;
+        }
+    }else if (current_thread == lock->exclusive_owner_thread){ //检查锁是否为当前线程持有
+        lock->state += acquires; //锁重入
+        return 1;
+    }
+    return 0;
+}
+
+static int nonfairTryAcquire(LOCK,ACQUIRES){
+    pid_t current_thread = getpid();
+    if(lock->state == 0){ //当前锁没有枷锁
+        //再次检查是否有其他线程抢占锁
+        if(compareAndSetState(&lock->cas,0,acquires)){
+            lock->exclusive_owner_thread = getpid();
+            return 1;
+        }
+    }else if (current_thread == lock->exclusive_owner_thread){ //检查锁是否为当前线程持有
+        lock->state += acquires; //锁重入
+        return 1;
+    }
+    return 0;
+}
+
+static int hasQueuedPredecessors(LOCK){
+    LOCK_NODE *t = lock->tail;
+    LOCK_NODE *h = lock->head;
+    LOCK_NODE *s;
+    //等待队列不为空，并且等待队列只有一个node，或者第一个等待node不是当前线程时
+    return h != t && 
+        ((s = h->next) == NULL || *(h->thread) != getpid());
+}
+
+static int acquireQueued(LOCK,NODE,ACQUIRES){
+    int interrupted = 0;
+    while (1){
+        LOCK_NODE *p = _predecessor();
+        if(p == lock->head && _tryAcquire()){ //如果第一个等待的节点是头节点，则重新尝试获取锁
+            //当前节点获取到锁
+            //当前节点晋升为head节点（空节点）
+            lock->head = node;
+            node->thread = NULL;
+            node->prev = NULL;
+            free(node->next);//释放老的prev节点空间
+            return interrupted;
+        }  
+        if(shouldParkAfterFailedAcquire(p,node) && 
+            parkAndCheckInterrupt()){
+            interrupted = 1;
+        }
+    }
+    if(1){
+        
+    }
+    
+}
+
+LOCK_NODE *initNode(){
+    LOCK_NODE *node = malloc(sizeof(LOCK_NODE));
+
+    node->predecessor = &predecessor;
+}
+
+LOCK_NODE *predecessor(NODE){
+    return node->prev;
+}
+
+static int shouldParkAfterFailedAcquire(LOCK_NODE *pred, LOCK_NODE *node){
+    int ws = pred->wait_status;
+    if(ws == SIGNAL){
+        //SIGNAL状态的的node正在等待被唤醒，可以安全挂起
+        return 1;
+    }else if(ws > 0){
+        //CANCELLED
+        //当前节点状态为取消，查找之前不是CANCELLED的节点，并淘汰CANCELLED的节点
+        do {
+            node->prev = pred = pred->prev;
+        }while (pred->wait_status > 0);
+        pred->next = node;
+    }else {
+        compareAndSetState(&pred->wait_status,ws,SIGNAL);
+    }
+    return 0;
+}
+
+static int parkAndCheckInterrupt(){
+
+    return 0;
+}
---
+++ ReentrantLock.h
@@ -0,0 +1,55 @@
+#ifndef __REENTRANTLOCK_H__
+#define __REENTRANTLOCK_H__
+
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define LOCK REENTRANT_LOCK *lock
+#define NODE LOCK_NODE *node
+#define ACQUIRES int acquires
+#define IS_FAIR lock->is_fair
+
+#define _predecessor() node->predecessor(node)
+#define _tryAcquire() tryAcquire(lock,acquires)
+
+enum node_status {
+    CANCELLED = 1,
+    SIGNAL = -1,
+    CONDITION = -2,
+    PROPAGATE = -3
+};
+
+/*
+*      +------+  prev +-----+       +-----+
+* head |      | <---- |     | <---- |     |  tail
+*      +------+       +-----+       +-----+
+*/
+typedef struct lock_node_struct{
+    struct lock_node_struct *prev;
+    struct lock_node_struct *next;
+    pid_t *thread;
+    enum node_status wait_status;
+    struct lock_node_struct *(* predecessor)(NODE);
+} LOCK_NODE;
+
+typedef struct reentrant_lock_struct{
+    int cas;
+    int is_fair;
+    pid_t exclusive_owner_thread;
+    int state;
+    LOCK_NODE *tail;
+    LOCK_NODE *head;
+} REENTRANT_LOCK;
+
+
+
+void lock(LOCK);
+
+void tryLock(LOCK);
+
+void unlock(LOCK);
+
+
+#endif
