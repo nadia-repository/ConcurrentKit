0427a669d7b4a42e84abb4675aaaf6a54d67d395
shixiang <xiang.shi@oriente.com>
2020-12-20 14:52:15 +0800

    feat: define thread struce & function

--- thread.c
+++ thread.c
@@ -1,2 +1,12 @@
 #include "thread.h"
 
+THREAD *currentThread(void){
+
+}
+
+void *runThread(THREAD *thread){
+    if(thread->is_self == 0){
+        //执行run方法时不为当前线程本身，则执行
+        
+    }
+}
--- thread.h
+++ thread.h
@@ -2,7 +2,14 @@
 
 typedef struct thread_struct{
     int pid;
+    char *name;
+    int priority;
+    int is_self;
+    void (* target)(void *arg);
 
+    struct thread_struct *next;
 } THREAD;
 
 THREAD *currentThread(void);
+
+void *runThread(THREAD *thread);
